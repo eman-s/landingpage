06adc1f393240413f117a0e18a34574925f94b11
Emmanuel Salicrup <emmanuel_salicrup@live.com>
2017-11-25 14:00:55 -0500

    added download functionality

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -3,11 +3,22 @@ import '../App.css';
 
 export default class BaseLayout extends Component {
   render() {
+    let style = {
+      'backgroundColor':'#092E20',
+      'backgroundSize':'cover'
+    }
     return (
-      <div className="Nav">
+      <div style={style} className="Nav">
         <div>
         <img src="./images/jameson-logo.jpg" alt='jameson' />
         <h1>You are without fear!</h1>
+        <span>Follow Us Here:</span>
+        <a href='https://www.instagram.com/jameson_us/'>
+          <img src="./images/instagram.png" alt='instagram' height='30px'/>
+        </a>
+        <a href='https://www.facebook.com/JamesonWhiskeyUS/'>
+          <img src="./images/facebook.png" alt='instagram' height='30px'/>
+        </a>
         </div>
         <section>
         {this.props.children}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 
 export default class Home extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      userimg:'./images/userimage.jpg'
+    }
+  }
+
+  postToFacebook(){
+
+  }
   render(){
     let style = {
       'backgroundColor':'#092E20',
       'backgroundSize':'cover'
     }
+    let imgHeight = {
+      'height':'25rem'
+    }
     return(
       <div style={style}>
       <section>
-        <img src='./images/userimage.jpg' alt='user' />
+        <img style={imgHeight} src={this.state.userimg} alt='user' />
       </section>
+      <section>
+        <a href={this.state.userimg} download='userimg'>Download!</a>
+      </section>
+
       </div>
     )
   }
